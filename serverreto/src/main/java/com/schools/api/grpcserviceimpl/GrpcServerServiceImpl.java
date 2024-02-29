ebbcf38dedc1afa5378c7582f86e434214bd7b5c
luarredo <luarredo@bancolombia.com>
2024-02-28 19:52:32 -0500

    Dockerfile adjusted and wornking

--- serverreto/src/main/java/com/schools/api/grpcserviceimpl/GrpcServerServiceImpl.java
+++ serverreto/src/main/java/com/schools/api/grpcserviceimpl/GrpcServerServiceImpl.java
@@ -7,7 +7,7 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class GrpcSchoolsServiceImpl extends serverServiceGrpc.serverServiceImplBase {
+public class GrpcServerServiceImpl extends serverServiceGrpc.serverServiceImplBase {
 
     @Override
     public void hello(Empty request, StreamObserver<ResponseServer> responseObserver) {
