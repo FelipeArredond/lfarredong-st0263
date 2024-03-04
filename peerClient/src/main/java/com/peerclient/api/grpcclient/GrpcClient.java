5b0e4fc038ffd8617cf48699b8d0d444466d7615
luarredo <luarredo@bancolombia.com>
2024-03-04 17:31:09 -0500

    Fixed pods communication and updated peers

--- peerClient/src/main/java/com/peerclient/api/grpcclient/GrpcClient.java
+++ peerClient/src/main/java/com/peerclient/api/grpcclient/GrpcClient.java
@@ -3,13 +3,17 @@ package com.peerclient.api.grpcclient;
 import com.server.grpc.*;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GrpcClient {
 
+    @Value("${service.grpc.host}")
+    private String hostName;
+
     public String getServerMessage(){
-        ManagedChannel channel = NettyChannelBuilder.forTarget("dns:///localhost:9091").usePlaintext().build();
+        ManagedChannel channel = NettyChannelBuilder.forTarget(hostName + ":9091").usePlaintext().build();
         serverServiceGrpc.serverServiceBlockingStub stub = serverServiceGrpc.newBlockingStub(channel);
         String response = stub.hello(Empty.newBuilder().build()).getResponseMessage();
         channel.shutdown();
@@ -18,7 +22,7 @@ public class GrpcClient {
     }
 
     public String reportFile(FileReport fileReport){
-        ManagedChannel channel = NettyChannelBuilder.forTarget("dns:///localhost:9091").usePlaintext().build();
+        ManagedChannel channel = NettyChannelBuilder.forTarget(hostName + ":9091").usePlaintext().build();
         serverServiceGrpc.serverServiceBlockingStub stub = serverServiceGrpc.newBlockingStub(channel);
         String response = stub.reportFile(fileReport).getResponseMessage();
         channel.shutdown();
@@ -27,7 +31,7 @@ public class GrpcClient {
     }
 
     public PeerWithFile peerWithFile(FileName fileName){
-        ManagedChannel channel = NettyChannelBuilder.forTarget("dns:///localhost:9091").usePlaintext().build();
+        ManagedChannel channel = NettyChannelBuilder.forTarget(hostName +  ":9091").usePlaintext().build();
         serverServiceGrpc.serverServiceBlockingStub stub = serverServiceGrpc.newBlockingStub(channel);
         PeerWithFile response = stub.getFileLocation(fileName);
         channel.shutdown();
